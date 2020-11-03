3f3701dbec6b8fe54e99725ded1ab85b6aeeb156
jake <pjklogs@naver.com>
2020-11-03 18:58:34 +0900

    - nodemon 추가

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ const port = 4000;
 const mongoose = require("mongoose");
 const keyString = require("./_key"); // Local Only
 
+const { User } = require("./model/User");
+
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: true })); // application/x-www-form-urlencoded 가져오기
+
+app.use(bodyParser.json()); // application/json 가져오기
+
 mongoose
   .connect(keyString, {
     useNewUrlParser: true,
@@ -18,7 +26,19 @@ mongoose
   .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("# Hello World! #");
+});
+
+app.post("/register", (req, res) => {
+  const user = new User(req.body);
+  user.save((err, userInfo) => {
+    if (err) {
+      return res.json({ success: false, err });
+    }
+    return res.status(200).json({
+      success: true
+    });
+  });
 });
 
 app.listen(port, () => {
--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
+# Training Content
+
 1. Express.js 설치
 
 2. MongoDB 세팅
@@ -8,3 +10,23 @@
 - `mongoose` 패키지 추가
 
 3. User 모델, User schema 작성
+
+- model > User.js
+
+4. git 초기화
+
+- `git init`으로 로컬 레포지토리 생성
+- `git status`로 확인
+- `git add .` 전체 add
+- `.gitignore` || `git rm --cached node_modules -r`
+- `Working directory` / `Staging Area` / `Git repository (local / remote)`
+
+- `ssh-keygen -t rsa -b 4096 -C "${userEmail@gmail.com}"`
+- ssh-agent 를 백그라운드에서 켜줘야함 `eval "${ssh-agent -s}"` `ssh-agent -s`
+- `ssh-add -K ~/.ssh/id_rsa`
+- `pbcopy < ~/.ssh/id_ras.pub` 클립보드에 퍼블릭 키 복사
+
+5. 회원가입 / Body Parser / Postman
+
+- `body-parser`
+- API 테스트 > 클라이언트 역활: Postman
